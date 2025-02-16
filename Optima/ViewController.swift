9537842d11919fbb659b6d5106f1825d6a2a5c4b
Ghada-Abdelrahman <ghada186@icloud.com>
2025-02-17 01:44:45 +0200

    Initial prolect setup

---
+++ Optima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Optima
+//
+//  Created by Ghada Abdelrahman on 17/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OptimaTests/OptimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  OptimaTests.swift
+//  OptimaTests
+//
+//  Created by Ghada Abdelrahman on 17/02/2025.
+//
+
+import Testing
+@testable import Optima
+
+struct OptimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
